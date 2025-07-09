020ef329d48f097bf22d9e679343baf9b8252d62
Christopher Bryan <christopher_bryan@rocketmail.com>
2025-07-09 09:51:02 -0700

    updated base url fetch condition with vercel_env

--- utils/url.ts
+++ utils/url.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
-  process.env.NEXT_PUBLIC_VERCEL_ENV === "development"
+  process.env.NEXT_PUBLIC_VERCEL_ENV !== "production"
     ? process.env.NEXT_PUBLIC_VERCEL_URL
     : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
